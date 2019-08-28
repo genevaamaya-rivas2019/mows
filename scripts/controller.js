4ca488243b1dd8a35f51eb2fbbc43b95e6dcdb13
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-08-28 16:06:19 +0800

    Added new functions

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,17 +1,57 @@
 // basic functionalities
-client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
-client.subscribe("mqtt/demo")
+var btnConnect = document.getElementById("btn-connect");
+var btnDisconnect = document.getElementById("btn-disconnect");
 
-client.on("connect", function(){
+//disconnect
+btnDisconnect.addEventListener("click", function(e){
+  e.preventDefault();
+  let status = document.getElementById("inpStatus");
+  status.value = "Disconnected!"
+})
+
+//connect
+btnConnect.addEventListener("click", function(e){
+  e.preventDefault();
+
+  let status = document.getElementById("inpStatus");
+  console.log("working Connect Button");
+  client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt") // connect to broker
+  client.subscribe("mqtt/demo") //subscribe sa topic
+
+  client.on("connect", function(){
     console.log("Successfully connected");
+    status.value = "Connected!"
+  })
+
+  client.on("message", function (topic, payload) { //triggered and display the message
+    console.log([topic, payload].join(": "));
+    //client.end();//equals disconnect
+  })
+
+  client.publish("mqtt/demo", "hello world!")
 })
 
-client.on("message", function (topic, payload) {
-  console.log([topic, payload].join(": "));
-  client.end();
+var payload1 = document.getElementById("pubPayload").value;
+//publish
+document.getElementById("btn-publish").addEventListener("click", function(e){
+  e.preventDefault();
+  client.on("message", function (topic, payload) { //triggered and display the message
+    console.log([topic, payload].join(": "));
+    //client.end();//equals disconnect
+  })
+  client.publish("mqtt/demo", payload1)
 })
 
-client.publish("mqtt/demo", "hello world!")
+document.getElementById("btn-subscribe").addEventListener("click",function(e){
+  e.preventDefault();
+  client.on("message", function (topic, payload) { //triggered and display the message
+    console.log([topic, payload].join(": "));
+    //client.end();//equals disconnect
+  })
+  client.subscribe("mqtt/demo")
+})
+
+
 
 // // advance functionalities
 // client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
